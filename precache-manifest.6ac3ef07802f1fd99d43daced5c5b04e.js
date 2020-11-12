58f823d2790c481064163eb467b134ed83be4d99
kamran subhani <itskamransubhani@gmail.com>
2020-11-12 19:22:38 +0500

    Updates

---
+++ precache-manifest.6ac3ef07802f1fd99d43daced5c5b04e.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c295ef3de4007879beec480affaf39",
+    "url": "/protest/index.html"
+  },
+  {
+    "revision": "9d848411088c6e835916",
+    "url": "/protest/static/css/2.e2e85d32.chunk.css"
+  },
+  {
+    "revision": "b25c88a20a708b9c94b8",
+    "url": "/protest/static/css/main.e697e1cc.chunk.css"
+  },
+  {
+    "revision": "9d848411088c6e835916",
+    "url": "/protest/static/js/2.d2de8a15.chunk.js"
+  },
+  {
+    "revision": "2989bd5bacee6a21e56c87e6446a7ba6",
+    "url": "/protest/static/js/2.d2de8a15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b25c88a20a708b9c94b8",
+    "url": "/protest/static/js/main.4180ee6e.chunk.js"
+  },
+  {
+    "revision": "a5727ed44eb582150cb4",
+    "url": "/protest/static/js/runtime-main.0030bab8.js"
+  },
+  {
+    "revision": "105f79771243c99c1f32389605873c09",
+    "url": "/protest/static/media/MobileBg.105f7977.jpg"
+  },
+  {
+    "revision": "1a8edd8ea457e3faf050282a7d903e9f",
+    "url": "/protest/static/media/MyFav_empty.1a8edd8e.svg"
+  },
+  {
+    "revision": "a64bdebcd10eae9628fec050899ab7dc",
+    "url": "/protest/static/media/MyFav_empty.a64bdebc.svg"
+  },
+  {
+    "revision": "249d8eed18e9090b0dbaeb3aa16d610d",
+    "url": "/protest/static/media/MyFav_filled.249d8eed.svg"
+  },
+  {
+    "revision": "63d9f60502d2558ad1ef051506f7925e",
+    "url": "/protest/static/media/MyFav_filled.63d9f605.svg"
+  },
+  {
+    "revision": "83419e152e7ae47ff1345f00106bd527",
+    "url": "/protest/static/media/MyWorkout_Filled.83419e15.svg"
+  },
+  {
+    "revision": "52668a59d80a5fbcb2f1b23a4e653736",
+    "url": "/protest/static/media/MyWorkout_empty.52668a59.svg"
+  },
+  {
+    "revision": "c2837619b13341b54f5e8e7df0758b07",
+    "url": "/protest/static/media/MyWorkout_empty.c2837619.svg"
+  },
+  {
+    "revision": "0b4228824486444642419e5780bae90b",
+    "url": "/protest/static/media/MyWorkout_filled.0b422882.svg"
+  },
+  {
+    "revision": "134b7d9b0b7a8fd93eda26954cb9db05",
+    "url": "/protest/static/media/allDrills.134b7d9b.png"
+  },
+  {
+    "revision": "06a08e50d0825f9ed1f77cd3c34e4aa9",
+    "url": "/protest/static/media/banner.06a08e50.png"
+  },
+  {
+    "revision": "eeb66e7dfd4d3965d93cc28a5baebfee",
+    "url": "/protest/static/media/baseballDrills.eeb66e7d.png"
+  },
+  {
+    "revision": "456d7c4c0f7be7b0f812033628a2e6c8",
+    "url": "/protest/static/media/baseballPic.456d7c4c.png"
+  },
+  {
+    "revision": "c22a9f220b38216ef07629a968f866e7",
+    "url": "/protest/static/media/basketBalPic.c22a9f22.png"
+  },
+  {
+    "revision": "3fb6c6407a4a13ae5d972ec684448ead",
+    "url": "/protest/static/media/basketBallImg.3fb6c640.png"
+  },
+  {
+    "revision": "689663a2e97e1c8e83bfdf6b5a212d2b",
+    "url": "/protest/static/media/bodybuilding.689663a2.png"
+  },
+  {
+    "revision": "ca969e9cb69e28c51690990c523062da",
+    "url": "/protest/static/media/btmSl1.ca969e9c.png"
+  },
+  {
+    "revision": "25e5cdd7160455fdc94e53d81008904e",
+    "url": "/protest/static/media/btmSl2.25e5cdd7.png"
+  },
+  {
+    "revision": "0559ab47e99c8f4f1ee838909f948b9d",
+    "url": "/protest/static/media/btmSl3.0559ab47.png"
+  },
+  {
+    "revision": "41ee5691d3a57ad9b679b4aee7f4af25",
+    "url": "/protest/static/media/contactBg.41ee5691.png"
+  },
+  {
+    "revision": "410c32b0c337321f82e236b1995d32d2",
+    "url": "/protest/static/media/drillsImg.410c32b0.png"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/protest/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/protest/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/protest/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "f721466883998665b87923b92dea655b",
+    "url": "/protest/static/media/glyphicons-halflings-regular.f7214668.svg"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/protest/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "25f73b5e054d822b9311b204963ef6b8",
+    "url": "/protest/static/media/gymImg.25f73b5e.png"
+  },
+  {
+    "revision": "a3842973a730a3284ba2b8a1297b81d3",
+    "url": "/protest/static/media/gymnastic.a3842973.png"
+  },
+  {
+    "revision": "303cf5bd9b88a6f0c77d267cdb16a886",
+    "url": "/protest/static/media/loginBg.303cf5bd.png"
+  },
+  {
+    "revision": "9b423355ab82e287e2761736fb678b6c",
+    "url": "/protest/static/media/loginBg.9b423355.png"
+  },
+  {
+    "revision": "e1eaf696641f25dcb0ea00a1d896c86b",
+    "url": "/protest/static/media/loginPopup.e1eaf696.png"
+  },
+  {
+    "revision": "f73b8fc2cc17e3714b9f69083dd9b4bd",
+    "url": "/protest/static/media/loginPopupSm.f73b8fc2.png"
+  },
+  {
+    "revision": "ebe776378b522dd238f08e9de09b9d29",
+    "url": "/protest/static/media/mainInnerBg.ebe77637.png"
+  },
+  {
+    "revision": "b10860f54485d8c2a16f25612a044299",
+    "url": "/protest/static/media/pic1.b10860f5.png"
+  },
+  {
+    "revision": "65fdaee777f1b86f3b383879360a1dd7",
+    "url": "/protest/static/media/pic2.65fdaee7.png"
+  },
+  {
+    "revision": "89943bd55b10a694dddc2a0b8d7b5e8b",
+    "url": "/protest/static/media/pic3.89943bd5.png"
+  },
+  {
+    "revision": "a953a6e5ffdbc088b240984dd566563d",
+    "url": "/protest/static/media/powerfulguy.a953a6e5.png"
+  },
+  {
+    "revision": "93644372e60b5b9d842062a07b26cce9",
+    "url": "/protest/static/media/profileInnerTopBg.93644372.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/protest/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/protest/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/protest/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/protest/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "927144d1265a361cb4ff8c7a1dc20751",
+    "url": "/protest/static/media/slider1.927144d1.png"
+  },
+  {
+    "revision": "b2763385bd26b7eec0e7d8ac77ecd95e",
+    "url": "/protest/static/media/testimonials.b2763385.png"
+  },
+  {
+    "revision": "b330ff8470237ce3067cdf3a45cffce4",
+    "url": "/protest/static/media/thumbnail.b330ff84.png"
+  },
+  {
+    "revision": "900fc3f0cc54d71a3a9e021c1ea20851",
+    "url": "/protest/static/media/video.900fc3f0.png"
+  }
+]);
